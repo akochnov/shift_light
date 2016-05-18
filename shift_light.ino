e055838415f08a514b6c45ed94e4818462861d68
AKochnov <aleksey.kochnov@gmail.com>
2016-05-18 17:27:10 +0300

    rpm spikes filter added. Switch rpm potentiometer added.

--- shift_light.ino
+++ shift_light.ino
@@ -1,71 +1,106 @@
-#define RPMpin 2 // датчик Холла
-long microsold = 0;
-int rpm = 0;
-int lastRpm = 0;
+#define rpmPin 2 // датчик Холла
+
+const int tunePin = A0;
+
+long microsPrev = 0;
+double rpmCur = 0;
+double rpmPrev = 0;
 int spikesCounter = 0;
-bool DEBUG = true;
+
 
 void setup() { 
-  Serial.begin(57600);
-  digitalWrite(RPMpin, 1);
-  attachInterrupt(0, RPM, FALLING);
+  Serial.begin(9600);
+  digitalWrite(rpmPin, 1);
+  attachInterrupt(0, getRpm, RISING);
 
-  for (int i = 5; i < 9; i++)
+  for (int i = 3; i < 9; i++)
   {
     pinMode(i, OUTPUT);
   }
 }
 
-void debugPrint(){
-    Serial.print("rpm = ");
-    Serial.println(rpm, DEC); // об/мин
-    
-    Serial.print("lastRpm = ");
-    Serial.println(lastRpm);
-    int diff = abs(lastRpm - rpm);
-       
-    Serial.print("diff = ");
-    Serial.println(diff);
 
-    Serial.print("SPIKE COUNTER = ");
-    Serial.println(spikesCounter);  
+void loop() 
+{
+ 
+  int shiftRpm = getShiftThreshold();
+  Serial.println(shiftRpm);
+  showRpm(rpmCur, shiftRpm);
+
+  
 }
 
-void loop() {
-    int diff = abs(lastRpm - rpm);
-       
-    if (diff > 300 && spikesCounter < 3){
-      //debugPrint();
-      spikesCounter++;
-      rpm = lastRpm;
-    } 
-    else 
+int getShiftThreshold()
+{
+  const int potentiometerPin = A0;
+
+  int sensorValue = analogRead(potentiometerPin);
+  sensorValue = constrain(sensorValue, 0, 1023);
+  return map(sensorValue, 0, 1023, 3000, 9000);
+}
+
+
+
+void showRpm(double rpm, int shiftRpm)
+{
+    const double rpmMin = 800;
+    const double ledMax = 1536;             //6 leds x 256
+    int digitalValue;
+    digitalValue = (int)constrain(ledMax / shiftRpm * rpm, 0, ledMax);
+
+    for (int i = 3; i < 9; i++)
     {
-      if (spikesCounter > 0) 
-      {
-        spikesCounter--;  
-      }
-      else
-      {
-        spikesCounter = 0;
-      }
+      digitalWrite(i, constrain(digitalValue, 0, 255));
+      digitalValue = digitalValue - 256;
     }
 
-
-    Serial.println(rpm, DEC); // об/мин
-   
     
-    if (rpm > 2000) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
-    if (rpm > 3000) {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
-    if (rpm > 4000) {digitalWrite(7, HIGH);} else {digitalWrite(7, LOW);}
-    if (rpm > 5000) {digitalWrite(8, HIGH);} else {digitalWrite(8, LOW);}
-    
-    
-    lastRpm = rpm;
-    //delay(10);
-
+    //Serial.println(digitalValue, DEC);
+    /*
+    if (rpm > 2000) {digitalWrite(3, HIGH);} else {digitalWrite(3, LOW);}
+    if (rpm > 3000) {digitalWrite(4, HIGH);} else {digitalWrite(4, LOW);}
+    if (rpm > 4000) {digitalWrite(5, HIGH);} else {digitalWrite(5, LOW);}
+    if (rpm > 5000) {digitalWrite(6, HIGH);} else {digitalWrite(6, LOW);}
+    if (rpm > 6000) {digitalWrite(7, HIGH);} else {digitalWrite(7, LOW);}
+    if (rpm > 7000) {digitalWrite(8, HIGH);} else {digitalWrite(8, LOW);}
+    */
+    //Serial.println(rpm);
 }
-void RPM (){
-  rpm = (1000000.0/(micros() - microsold))*60;
-  microsold = micros();
+
+bool checkForSpike(double rpm1, double rpm2)
+{
+  const int spikeThreshold = 200;
+  
+  return abs(rpm1 - rpm2) > spikeThreshold;
+}
+
+void getRpm()
+{
+  const int maxSpikes = 5;                   
+  
+  rpmCur = (1000000.0/(micros() - microsPrev))*60;
+  
+  //Catch spikes
+  bool isSpike = checkForSpike(rpmCur, rpmPrev);
+  if (isSpike) 
+  {
+    if (spikesCounter < maxSpikes)           //More than XX spikes in a row considered as good value
+    {
+      spikesCounter++;
+      rpmCur = rpmPrev;                              //Take previous rpm value in case spike is detected
+    }
+    else
+    {
+      spikesCounter = 0;
+    }
+  }
+  else
+  {
+    spikesCounter = 0;
+  }
+  
+  microsPrev = micros();
+  rpmPrev = rpmCur;
+  //Serial.println(rpm);
+  
 }
